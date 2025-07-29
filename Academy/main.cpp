bb62bad83698db8f5082383ed2251032ef1e30f7
vivivych <ivychhh@yandex.ru>
2025-07-29 12:23:17 +0300

    academy

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<string>
-//using namespace std; 
+//using namespace std;
 using std::cin;
 using std::cout;
 using std::endl;
@@ -12,10 +12,20 @@ using std::endl;
 
 class Human
 {
+
+	static const int LAST_NAME_WIDTH = 15;
+	static const int FIRST_NAME_WIDTH = 15;
+	static const int AGE_WIDTH = 3;
+	static int count;		//static member declaration
 	std::string last_name;
 	std::string first_name;
 	int age;
+
 public:
+	static int get_count()
+	{
+		return count;
+	}
 	const std::string& get_last_name()const
 	{
 		return last_name;
@@ -47,29 +57,52 @@ public:
 		set_last_name(last_name);
 		set_first_name(first_name);
 		set_age(age);
+		count++;
 		cout << "HConstrutor:\t" << this << endl;
 	}
 	virtual ~Human()
 	{
+		count--;
 		cout << "HDestructor:\t" << this << endl;
 	}
 
 	//				  Methods:
-	virtual void info()const
+	virtual std::ostream& info(std::ostream& os)const
 	{
-		cout << last_name << " " << first_name << " " << age << endl;
+		//return os << last_name << " " << first_name << " " << age;
+		os.width(LAST_NAME_WIDTH);	//метод width(N) задает ширину вывода в 'N' знакопозиций,
+		//если выводима€ строка меньше 'N', то недостающие символы заполн€ютс€ пробелами,
+		//если выводима€ строка больше 'N', она выводитс€ полностью, и общее выравнивание нарушаетс€
+		os << std::left;//первый вызов width() задает выравнивание по правому краю заданного пол€, 
+		//и его нужно €вно выровн€ть по левому краю.
+		os << last_name;
+		os.width(FIRST_NAME_WIDTH);
+		os << first_name;
+		os.width(AGE_WIDTH);
+		os << age;
+		return os;
 	}
 };
+
+int Human::count = 0;	//—татические переменные можно инициализировать только за пределами класса.
+std::ostream& operator<<(std::ostream& os, const Human& obj)
+{
+	return obj.info(os);
+	//return os << obj.get_last_name() << " " << obj.get_first_name() << " " << obj.get_age();
+}
 
 #define STUDENT_TAKE_PARAMETERS const std::string& speciality, const std::string& group, double rating, double attendance
 #define STUDENT_GIVE_PARAMETERS speciality, group, rating, attendance
 
 class Student :public Human
 {
+	static const int SPECIALITY_WIDTH = 22;
+	static const int GROUP_WIDTH = 8;
+	static const int RATING_WIDTH = 5;
 	std::string speciality;
 	std::string group;
-	double rating;		//успеваемость
-	double attendance;	//посещаемость
+	double rating;		
+	double attendance;	
 public:
 	const std::string& get_speciality()const
 	{
@@ -104,7 +137,7 @@ public:
 		this->attendance = attendance;
 	}
 
-	//				/Constructors:
+	//				Constructors:
 	Student
 	(
 		HUMAN_TAKE_PARAMETERS,
@@ -123,10 +156,21 @@ public:
 	}
 
 	//					Methods:
-	void info()const override
+	std::ostream& info(std::ostream& os)const override
 	{
-		Human::info();
-		cout << speciality << " " << group << " " << rating << " " << attendance << endl;
+		Human::info(os);
+		os.width(SPECIALITY_WIDTH);
+		os << speciality;
+		os.width(GROUP_WIDTH);
+		os << group;
+		os.width(RATING_WIDTH);
+		os << rating;
+		os.width(RATING_WIDTH);
+		os << attendance;
+		return os;
+
+		//Human::info(os) << " ";
+		//return os << speciality << " " << group << " " << rating << " " << attendance;
 	}
 };
 
@@ -135,6 +179,8 @@ public:
 
 class Teacher :public Human
 {
+	static const int SPECIALITY_WIDTH = 22;
+	static const int EXPERIENCE_WIDTH = 3;
 	std::string speciality;
 	int experience;
 public:
@@ -171,11 +217,17 @@ public:
 		cout << "TDestructor:\t" << this << endl;
 	}
 
-	//			/	Methods:
-	void info()const override
+	//				Methods:
+	std::ostream& info(std::ostream& os)const override
 	{
-		Human::info();
-		cout << speciality << " " << experience << endl;
+		Human::info(os);
+		os.width(SPECIALITY_WIDTH);
+		os << speciality;
+		os.width(EXPERIENCE_WIDTH);
+		os << experience;
+		return os;
+		//Human::info(os) << " ";
+		//return os << speciality << " " << experience;
 	}
 };
 
@@ -193,15 +245,15 @@ public:
 	{
 		cout << "GDestructor:\t" << this << endl;
 	}
-	void info()const override
+	std::ostream& info(std::ostream& os)const override
 	{
-		Student::info();
-		cout << subject << endl;
+		Student::info(os) << " ";
+		return os << subject;
 	}
 };
 
 //#define INHERITANCE
-#define POLYMORPHISM	//poly - много, morphis - форма.//
+#define POLYMORPHISM
 
 void main()
 {
@@ -221,15 +273,6 @@ void main()
 	graduate.info();
 #endif // INHERITANCE
 
-	/*
-	---------------------------------------------------
-	1. Ad-hoc polymorphism;
-	2. Inclusion polymorphism (полиморфизм подтипов):
-		”казатели на базовый класс (Base class pointers): Generalisation, Upcast;
-		¬иртуальные методы: vfptr (Virtual Function pointers) - таблица указателей на виртуальные функции;
-	   Runtime polymorphism;
-	---------------------------------------------------
-	*/
 
 	Human* group[] =
 	{
@@ -239,16 +282,22 @@ void main()
 		new Teacher("White", "Walter", 50, "Chemistry", 25),
 		new Graduate("Schreder", "Hank", 40, "Criminalistic", "WW_220", 40, 60, "How to catch Heisenberg"),
 		new Student("Vercetty", "Tommy", 30, "Theft", "Vice", 98, 99),
-		new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20)
+		new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20),
+		new Graduate("Targarian", "Daineris", 22, "Flight", "GoT", 91, 92, "How to make smoke"),
+		new Teacher("Schwartzneger", "Arnold", 85, "Heavy Metal", 60)
 	};
 	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
 	{
-		group[i]->info();
+		//group[i]->info();
+		cout << *group[i] << endl;
 		cout << delimiter << endl;
 	}
+	cout << " оличество людей: " << group[0]->get_count() << endl;
 	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
 	{
 		delete group[i];
 	}
+	cout << " оличество людей: " << group[0]->get_count() << endl;
+	//cout << " оличество людей: " << Human::get_count() << endl;
 
 }
